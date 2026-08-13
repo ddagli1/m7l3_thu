0b4a4b7ca726fb67a3488b7aa4806959bb6ab26f
omerbey365 <cevikomerr@hotmail.com>
2026-08-13 20:59:43 +0300

    new add tertestor

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -1,13 +1,22 @@
 import string
 from password.new_password import generate_password
 
+
 def test_password_characters():
     """Şifre oluşturulurken yalnızca geçerli karakterlerin kullanıldığını test eder"""
     valid_characters = string.ascii_letters + string.digits + string.punctuation
-    password = generate_password(100)  # Daha güvenli bir doğrulama için uzun bir şifre oluşturuluyor
+    password = generate_password(100)
+
     for char in password:
         assert char in valid_characters
 
+def test_password_length():
+    password = generate_password(20)
+    assert len(password) == 20
+def test_passwords_are_different():
+    password1 = generate_password(20)
+    password2 = generate_password(20)
+    assert password1 != password2
 """
 Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
 Daha fazla test yazabilirseniz harika olur!
